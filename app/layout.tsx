5d45aab6a61f1419444ca96ff666f80f71f5dfb3
Andre Marshandito <andremarshandito@gmail.com>
2025-12-14 14:32:06 +0700

    Fix revisi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { NextAuthProvider } from "./providers";
 import "./globals.css";
 
 const geistSans = Geist({
